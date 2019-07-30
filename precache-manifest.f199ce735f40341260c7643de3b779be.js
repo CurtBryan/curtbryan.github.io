1bee6c1b2b20fb792e327e2f6caa44cf600954dd
Curt Bryan <kurdtbryan@gmail.com>
2019-07-30 12:29:47 -0700

    Updates

---
+++ precache-manifest.f199ce735f40341260c7643de3b779be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "566a3d5ff4f6043d4a382af9b3c48294",
+    "url": "/index.html"
+  },
+  {
+    "revision": "731c54182c192b66a458",
+    "url": "/static/css/main.98ef3ee3.chunk.css"
+  },
+  {
+    "revision": "9a166472c9365f90e84e",
+    "url": "/static/js/2.498f492d.chunk.js"
+  },
+  {
+    "revision": "731c54182c192b66a458",
+    "url": "/static/js/main.99920501.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
